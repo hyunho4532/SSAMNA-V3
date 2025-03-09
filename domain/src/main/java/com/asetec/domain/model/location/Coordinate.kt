602acebd1396af2a1f61704bdbc8ae70ab071d33
hyunho4532 <kyounguk192@gmail.com>
2025-03-09 20:04:22 +0900

    feat. coordinate data class step, km 변수 추가

--- domain/src/main/java/com/asetec/domain/model/location/Coordinate.kt
+++ domain/src/main/java/com/asetec/domain/model/location/Coordinate.kt
@@ -1,6 +1,5 @@
 package com.asetec.domain.model.location
 
-import com.google.gson.reflect.TypeToken
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -11,5 +10,8 @@ data class Coordinate(
     val googleId: String = "",
     val latitude: Double = 0.0,
     val longitude: Double = 0.0,
-    val altitude: Double = 0.0
+    val altitude: Double = 0.0,
+
+    val step: Int = 0,
+    val km: String = ""
 )
