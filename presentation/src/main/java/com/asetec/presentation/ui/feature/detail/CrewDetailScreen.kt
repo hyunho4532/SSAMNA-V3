967923046836219e8558675917ae9fc616b20ea0
hyunho4532 <kyounguk192@gmail.com>
2025-03-23 15:24:10 +0900

    feat. 크루 기능 구현 중 (알림 데이터 crew_id json parsing 완료)

--- presentation/src/main/java/com/asetec/presentation/ui/feature/detail/CrewDetailScreen.kt
+++ presentation/src/main/java/com/asetec/presentation/ui/feature/detail/CrewDetailScreen.kt
@@ -26,6 +26,12 @@ import androidx.navigation.compose.rememberNavController
 import com.asetec.domain.model.dto.CrewDTO
 import com.asetec.presentation.viewmodel.CrewViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 @Composable
 fun CrewDetailScreen(
@@ -38,36 +44,64 @@ fun CrewDetailScreen(
         crewViewModel.notificationAll()
     }
 
+    val notificationData = crewViewModel.notification.collectAsState()
+
+    val idx = notificationData.value.map {
+        it.crewId["idx"]
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        crewList.forEach {
-            val imageName = it.picture.replace("R.drawable.", "")
-            val imageResId = context.resources.getIdentifier(imageName, "drawable", context.packageName)
+        idx.forEach { jsonElement ->
+            if (jsonElement is JsonArray) {
+                jsonElement.forEach { item ->
+                    if (item is JsonObject) {
+                        val notificationCrewId = item["id"]?.jsonPrimitive?.contentOrNull!!.toInt()
 
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(260.dp)
-            ) {
-                Image(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    painter = painterResource(id = imageResId),
-                    contentDescription = "크루 상세 이미지",
-                    contentScale = ContentScale.Crop
-                )
-            }
+                        crewList.forEach { crew ->
+                            notificationData.value.forEach { notification ->
+                                if (crew.crewId == notificationCrewId) {
+                                    val imageName = crew.picture.replace("R.drawable.", "")
+                                    val imageResId = context.resources.getIdentifier(
+                                        imageName,
+                                        "drawable",
+                                        context.packageName
+                                    )
 
-            Box(
-                modifier = Modifier
-                    .padding(top = 12.dp, start = 6.dp)
-            ) {
-                Text(
-                    text = it.title,
-                    fontSize = 18.sp
-                )
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(260.dp)
+                                    ) {
+                                        Image(
+                                            modifier = Modifier
+                                                .fillMaxWidth(),
+                                            painter = painterResource(id = imageResId),
+                                            contentDescription = "크루 상세 이미지",
+                                            contentScale = ContentScale.Crop
+                                        )
+                                    }
+
+                                    Box(
+                                        modifier = Modifier
+                                            .padding(top = 12.dp, start = 6.dp)
+                                    ) {
+                                        Text(
+                                            text = crew.crewId.toString(),
+                                            fontSize = 18.sp
+                                        )
+                                    }
+
+                                    Text(
+                                        text = notification.title
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
