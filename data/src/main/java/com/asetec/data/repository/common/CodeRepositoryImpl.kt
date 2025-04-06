f053bce514124a498fd24caf42b9092bd2d97890
hyunho4532 <kyounguk192@gmail.com>
2025-04-06 15:56:02 +0900

    feat. common_code 공통 코드 MVVM 구현 완료

---
+++ data/src/main/java/com/asetec/data/repository/common/CodeRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.asetec.data.repository.common
+
+import com.asetec.domain.model.common.Code
+import com.asetec.domain.repository.common.CodeRepository
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.rpc
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class CodeRepositoryImpl @Inject constructor(
+    private val postgrest: Postgrest
+): CodeRepository {
+    override suspend fun select(): List<Code> {
+        return withContext(Dispatchers.IO) {
+            postgrest
+                .rpc("get_common_code", mapOf("groupkey" to "ACTIVATE_STATUS"))
+                .decodeList()
+        }
+    }
+}
