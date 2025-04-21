37d5fd8d15b768d047f80703407fec688f05dd61
hyunho4532 <kyounguk192@gmail.com>
2025-04-21 21:02:24 +0900

    feat. ChallengeMaster 모델 선언

---
+++ domain/src/main/java/com/app/domain/model/state/ChallengeMaster.kt
@@ -0,0 +1,25 @@
+package com.app.domain.model.state
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ChallengeMaster(
+    @SerialName("id")
+    val id: Int = 0,
+
+    @SerialName("name")
+    val name: String = "",
+
+    @SerialName("description")
+    val description: String = "",
+
+    @SerialName("assets")
+    val assets: String = "",
+
+    @SerialName("type")
+    val type: Int? = 0,
+
+    @SerialName("goal")
+    val goal: Int? = 0
+)
