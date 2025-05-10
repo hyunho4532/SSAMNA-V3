4a614e815b3eeb43001bcfc7c8e7608cc8724b71
hyunho4532 <kyounguk192@gmail.com>
2025-05-10 22:52:57 +0900

    feat. 사용자 설정 기능 구현 중 (다크 모드 기능 구현 중)

--- presentation/src/main/java/com/app/presentation/ui/feature/auth/SettingScreen.kt
+++ presentation/src/main/java/com/app/presentation/ui/feature/auth/SettingScreen.kt
@@ -1,9 +1,22 @@
 package com.app.presentation.ui.feature.auth
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.domain.model.enum.ButtonType
 import com.app.domain.model.user.User
+import com.app.presentation.component.tool.CustomButton
+import com.app.presentation.component.util.responsive.setUpWidth
 
 /**
  * 사용자 설정 화면
@@ -14,6 +27,24 @@ fun SettingScreen(
 ) {
     Column(
         modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 26.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = user.name,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        CustomButton(
+            type = ButtonType.EventStatus.DARKTHEME,
+            width = setUpWidth(),
+            height = 46.dp,
+            text = "다크 모드 활성화",
+            showIcon = true,
+            backgroundColor = Color(0xFF5c9afa),
+            shape = "Rectangle"
+        )
     }
 }
