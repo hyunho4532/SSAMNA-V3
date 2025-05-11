e4239f94236d2ddc27c5be46ed7d2a6f2a0a63f0
hyunho4532 <kyounguk192@gmail.com>
2025-05-11 18:31:22 +0900

    feat. 광고 배너 구현 완료 (StatueViewModel 테마 관련 상태 관리 선언)

--- presentation/src/main/java/com/app/presentation/viewmodel/StateViewModel.kt
+++ presentation/src/main/java/com/app/presentation/viewmodel/StateViewModel.kt
@@ -1,4 +1,19 @@
 package com.app.presentation.viewmodel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+
+/**
+ * 앱의 상태를 관리한다.
+ */
 class StateViewModel {
+    /**
+     * 테마를 관리한다.
+     */
+    private val _isDarkTheme = mutableStateOf(false)
+    val isDarkTheme: State<Boolean> = _isDarkTheme
+
+    fun toggleTheme() {
+        _isDarkTheme.value = !_isDarkTheme.value
+    }
 }
